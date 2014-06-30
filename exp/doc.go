ad045a16ae7e446d63b1b9a3dfa0b2eab64101ec
Vic Demuzere <vic@demuzere.be>
2014-06-30 11:56:33 +0200

    Client is a helper for creating an IRC client.

---
+++ exp/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2014 Vic Demuzere
+//
+// Use of this source code is governed by the MIT license.
+
+// Package exp holds experimental code.
+//
+// Warning: this code is experimental and unreliable. Some may
+// one day be moved to a new path, modified or even disappear entirely.
+package exp
